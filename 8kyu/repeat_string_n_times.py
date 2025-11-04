a9efd6706d236cd616ef8ae0918444283eaab208
Connor <connorware@outlook.com>
2025-11-04 13:27:33 +0000

    Add repeat_string_n_times.py

---
+++ 8kyu/repeat_string_n_times.py
@@ -0,0 +1,9 @@
+"""
+String Repeat
+
+Description:
+Write a function that accepts a non-negative integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+"""
+
+def repeat_str(n, s):
+    return s * n
