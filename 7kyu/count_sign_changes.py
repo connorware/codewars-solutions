543c80c429619f1e0acc0d28cea3d0b39f57bb69
Connor <connorware@outlook.com>
2025-11-04 11:32:19 +0000

    Add count_sign_changes.py

---
+++ 7kyu/count_sign_changes.py
@@ -0,0 +1,32 @@
+"""
+Plus - minus - plus - plus - ... - Count
+
+Description:
+Count how often sign changes in array.
+
+result
+number from 0 to ... . Empty array returns 0
+
+example
+const arr = [1, -3, -4, 0, 5];
+
+/*
+| elem | count |
+|------|-------|
+|  1   |  0    |
+| -3   |  1    |
+| -4   |  1    |
+|  0   |  2    |
+|  5   |  2    |
+*/
+
+catchSignChange(arr) == 2;
+"""
+
+def catch_sign_change(lst):
+    count = 0
+    for i in range(len(lst)-1): 
+        if (lst[i] < 0 and lst[i+1] >= 0) or  (lst[i] >= 0 and lst[i+1] < 0 ):
+            count = count + 1
+    return count
+
